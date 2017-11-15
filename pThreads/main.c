1ec6a0a26d100b4f4e567449966f9d069f19bb04
Jaka Kordež <jaka.kordez@gmail.com>
2017-11-15 18:43:30 +0100

    PThreads version added.

---
+++ pThreads/main.c
@@ -0,0 +1,21 @@
+#define HAVE_STRUCT_TIMESPEC
+
+#include <stdio.h>
+#include <pthread.h>
+
+void *hi(void *arg)
+{
+	printf("Hello world!");
+	return NULL;
+}
+
+int main(void)
+{
+	pthread_t nit;
+
+	pthread_create(&nit, NULL, hi, NULL);
+	pthread_join(nit, NULL);
+
+	return 0;
+}
+
