eb1a9e32c10e4e055af0cabe098504b0a51f9f0e
Jaka Kordez <jaka.kordez@gmail.com>
2017-11-16 21:43:14 +0100

    PThreads matrix normalization. Added 64bit library.

--- pThreads/chain.cpp
+++ pThreads/chain.cpp
@@ -1,15 +1,55 @@
-#include "stdafx.h"
+
 #include <math.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 #include "chain.h"
 #include "matrix.h"
+
+#define THREADS	2
 
 int fillMatrix(float * matrix, int *data, int n) {
 	// parallel stuff
 	return 1;
 }
 
+typedef struct _normalize_args {
+	int id;
+	float *matrix;
+} normalize_args;
+
+void *normalize_thread(void *arg)
+{
+	normalize_args *ar = (normalize_args *)arg;
+	int n = pow(STATES, PAST)/THREADS;
+	int start = n*ar->id;
+	int end = (n + 1)*ar->id;
+	for (int i = start; i < end; i++) {
+		float sum = 0;
+		for (int j = 0; j < STATES; j++) {
+			sum += ar->matrix[i*STATES + j];
+		}
+		if (sum == 0) continue;
+		for (int j = 0; j < STATES; j++) {
+			ar->matrix[i*STATES + j] /= sum;
+		}
+	}
+	free(arg);
+	return NULL;
+}
+
 void normalizeMatrix(float *matrix) {
-	// parallel stuff
+	pthread_t threads[THREADS];
+	
+	for (int i = 0; i < THREADS; i++) {
+		normalize_args *args = (normalize_args *)malloc(sizeof(normalize_args));
+		args->id = i;
+		args->matrix = matrix;
+		pthread_create(&threads[i], NULL, normalize_thread, (void *)args);
+	}
+	for (int i = 0; i < THREADS; i++) {
+		pthread_join(threads[i], NULL);
+	}
 }
 
--- pThreads/main.c
+++ pThreads/main.c
@@ -2,21 +2,30 @@
 
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 #include "matrix.h"
+#include "csv.h"
+#include "chain.h"
+#include "benchmark.h"
 
-void *hi(void *arg)
-{
-	printf("Hello world!");
-	return NULL;
-}
+float minPercentage = 1;
+float maxPercentage = 1;
 
 int main(void)
 {
-	pthread_t nit;
+	/*float *borders = getExponentialBorders();
 
-	pthread_create(&nit, NULL, hi, NULL);
-	pthread_join(nit, NULL);
+	float *values;
+
+	minuteTick *first = (minuteTick *)malloc(sizeof(minuteTick));
+	int sizeY = readCSV(dataFilename(), first, &minPercentage, &maxPercentage);
+	int sizeK = sizeY;
+
+	float *matrixY = allocate_matrix();
+	float *matrixK = allocate_matrix();
+
+	runBenchmark(30, matrixY, matrixK, sizeY, sizeK, borders, first);*/
 
 	return 0;
 }
